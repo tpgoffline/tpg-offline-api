1b6506f16c599358f8dcb5c569ea6a4f29c38490
Rémy Da Costa Faro <remy@dacostafaro.com>
2018-12-16 14:39:53 +0100

    Update repo

--- flask_server/config.py
+++ flask_server/config.py
@@ -5,3 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-really-never-guess'
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@host:port/tpgoffline-apns_apns"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSONIFY_PRETTYPRINT_REGULAR = False
+    DEBUG = True
